dd0ba63d545d7c60fe03c2eb6edd0e15610ce5e9
maduranga <maduranga.wimalarathne95@gmail.com>
2025-08-27 14:01:11 +0400

    type fix

--- StockSync.SupplierService/Controllers/AuthController.cs
+++ StockSync.SupplierService/Controllers/AuthController.cs
@@ -26,7 +26,7 @@ public class AuthController : ControllerBase
     [HttpGet("{userId}", Name = "GetUser")]
     public async Task<ActionResult<User>> GetUserById(string userId)
     {
-        var user = await _dbContext.Users.FindAsync(userId);
+        var user = await _dbContext.Users.FindAsync(int.Parse(userId));
         if (user == null)
         {
             return NotFound(new ProblemDetails
