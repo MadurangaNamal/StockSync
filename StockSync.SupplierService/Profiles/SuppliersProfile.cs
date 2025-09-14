81dc20aae04489a88baade85741596be79ea75b3
maduranga <maduranga.wimalarathne95@gmail.com>
2025-09-14 13:10:50 +0400

    Update mapping for supplier response from chache

--- StockSync.SupplierService/Profiles/SuppliersProfile.cs
+++ StockSync.SupplierService/Profiles/SuppliersProfile.cs
@@ -1,14 +1,18 @@
 ﻿using AutoMapper;
 using StockSync.SupplierService.Entities;
+using StockSync.SupplierService.Infrastructure;
 using StockSync.SupplierService.Models;
 
 namespace StockSync.SupplierService.Profiles;
 
 public class SuppliersProfile : Profile
 {
-    public SuppliersProfile()
+    public SuppliersProfile(ICacheService cacheService)
     {
-        CreateMap<Supplier, SupplierDto>();
+        CreateMap<Supplier, SupplierDto>()
+            .ForMember(dest => dest.Items, opt => opt.MapFrom((src, dest) =>
+                (src.Items != null ? src.Items.Select(itemId => cacheService.GetItemDto(itemId)).Where(item => item != null).ToList() : [])));
+
         CreateMap<SupplierManipulationDto, Supplier>();
     }
 }
